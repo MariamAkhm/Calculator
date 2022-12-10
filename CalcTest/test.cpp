01644b59d9e5b4a62e5654d8d8bb1bdf24e12812
MariamAkhm <mahmetsafina7@gmail.com>
2022-12-10 13:59:30 +0300

    немного прописаны тесты

---
+++ CalcTest/test.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#include "../Calculator/Token.h"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1+2+3, 6);
+  EXPECT_EQ((1 + 2) * 5 , 15);
+  EXPECT_EQ((10 + 20) / 2, 15);
+  EXPECT_TRUE(true);
+}
